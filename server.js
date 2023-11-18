67f6424279a22dd4b28bd57f409cf0ecbc0393c8
poemaman <luis.campos.pousa@gmail.com>
2023-11-18 09:44:04 +0100

    more

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const http = require('http');
+// const server = require('http');
 
-// Se encarga de escuchar las peticiones que lleguen al servidor.//
-//req = peticion res = respuesta//
-const requestHandler = (req, res) => {
-      console.log(req.url);
-      if (req.url == 'Hello') {
-      res.setHeader("Content-Type", "text.json");
-    res.writeHead(200);
-    res.write("¿Hola Cliente");
-    res.end();
-  }
+// Se encarga de escuchar las peticiones que lleguen al servidor sin express//
+// //req = peticion res = respuesta//
+// const requestHandler = (req, res) => {
+//       // console.log(req.url);
+//       // if (req.url == 'Hello') {
+//       // res.setHeader("Content-Type", "text.json");
+//     res.writeHead(200);
+//     res.write("¿Hola Cliente");
+//     res.end();
+//   }
     //   }
     //   if (req.url == '/Bye') {
     //     res.setHeader("Content-Type", "text.json");
@@ -23,11 +23,35 @@ const requestHandler = (req, res) => {
     // res.write("¿Buenas, el servidor esta fucnionando");
     // res.end('hurra');
     // console.log('hurra');
-};
+// };
 
 //Creacion del servidor//
 const PORT = 3000;
-const server = http.createServer(requestHandler);
+// const server = http.createServer(requestHandler);
+
+const express = require('express');
+const server = express();
+
+const router = express.Router();
+
+
+router.get('/',(req,res) => {
+  console.log('Se ha conectado a la API')
+  res.send('Hello Upgrade Hub!');
+});
+
+// server.use('/', (req, res) => {
+//   console.log('Se ha conectado a la API')
+//   res.send('Hello Upgrade Hub!');
+// });
+
+router.get('/movies',(req,res) => {
+// server.use('/movies', (req, res) => {
+	const movies = ['Harry Potter', 'Titanic', 'Back to the Future'];
+  res.send(movies);
+});
+
+server.use('/', router);
 
 server.listen(PORT, () => {
   console.log(`Server started in http://localhost:${PORT}`);
